53c77ea064d34fafeb8f26874523fee707d4e2e0
mj-dev <maxence.joncheray@gmail.com>
2018-02-13 18:50:23 +0100

    add access control origin

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# BrawlStar-Api
-An API to get info on the SuperCell game BrawlStar
+# Pokedex-Api
+An API to get info on the Pokemon game
--- index.js
+++ index.js
@@ -10,6 +10,12 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Pokemondb');
 // start with > mongod
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
