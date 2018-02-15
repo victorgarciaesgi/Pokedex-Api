f0eba4685d13fc102ca2f7c73880db3aa3007784
Gauthier <mysterandco@gmail.com>
2018-02-15 15:07:20 +0100

    Updated access /users with token for all routes

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -6,58 +6,68 @@ let mongoose = require('mongoose'),
 
 /** list users **/
 exports.list_all_users = function(req, res) {
-    //jwt.verify(req.token, config.secret, (err, users) => {
+    jwt.verify(req.token, config.secret, (err, users) => {
         User.find({}, function (err, users) {
             if (err) return res.status(500).send("Erreur : method list_all_users");
             res.status(200).send(users);
         });
-   // });
+   });
 };
 
 /** create user **/
 exports.create_user = function(req, res){
-    User.create({
-        name : req.body.name,
-        email : req.body.email,
-        password : req.body.password
-    },
-    function (err, user) {
-        if (err) return res.status(500).send("Erreur : method create_user");
-        res.status(200).send(user);
+    jwt.verify(req.token, config.secret, (err, user) => {
+        User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            },
+            function (err, user) {
+                if (err) return res.status(500).send("Erreur : method create_user");
+                res.status(200).send(user);
+            });
     });
 };
 
 /** display user **/
 exports.read_user = function(req, res){
-    User.findOne({name: req.params.name}, function (err, user) {
-        if (err) return res.status(500).send("Erreur : method read_user");
-        if (!user) return res.status(404).send("User non trouvé - method read_user");
-        res.status(200).send(user);
+    jwt.verify(req.token, config.secret, (err, user) => {
+        User.findOne({name: req.params.name}, function (err, user) {
+            if (err) return res.status(500).send("Erreur : method read_user");
+            if (!user) return res.status(404).send("User non trouvé - method read_user");
+            res.status(200).send(user);
+        });
     });
 };
 
 /** edit user **/
 exports.update_user = function (req, res) {
-    User.findOneAndUpdate({name: req.params.name}, req.body, {new: true}, function (err, user) {
-        if (err) return res.status(500).send("Erreur : method update_user");
-        res.status(200).send(user);
+    jwt.verify(req.token, config.secret, (err, user) => {
+        User.findOneAndUpdate({name: req.params.name}, req.body, {new: true}, function (err, user) {
+            if (err) return res.status(500).send("Erreur : method update_user");
+            res.status(200).send(user);
+        });
     });
 };
 
 /** delete user **/
 exports.delete_user = function(req, res) {
-    User.findOneAndRemove({name: req.params.name}, function (err, user) {
-        if (err) return res.status(500).send("Erreur : method delete_user");
-        res.status(200).send("User : "+ user.name +" supprimé.");
+    jwt.verify(req.token, config.secret, (err, user) => {
+        User.findOneAndRemove({name: req.params.name}, function (err, user) {
+            if (err) return res.status(500).send("Erreur : method delete_user");
+            res.status(200).send("User : " + user.name + " supprimé.");
+        });
     });
 };
 
 /** list pokemons user **/
 exports.list_pokemons_user = function(req, res){
-    User.findOne({name: req.params.name}, function (err, user) {
-        if (err) return res.status(500).send("Erreur : method list_pokemons_user");
-        if (!user) return res.status(404).send("User non trouvé - method list_pokemons_user");
-        res.status(200).send(user.pokemonsCatched);
+    jwt.verify(req.token, config.secret, (err, user) => {
+        User.findOne({name: req.params.name}, function (err, user) {
+            if (err) return res.status(500).send("Erreur : method list_pokemons_user");
+            if (!user) return res.status(404).send("User non trouvé - method list_pokemons_user");
+            res.status(200).send(user.pokemonsCatched);
+        });
     });
 };
 
@@ -96,3 +106,24 @@ exports.delete_pokemon_user = function(req, res){
         res.status(200).send("Pokemon supprimé");
     });
 };
+
+exports.verifyToken = function(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken;
+        // Next middleware
+        next();
+    } else {
+        // Forbidden
+        res.status(403).send("Accès refusé. Vous devez vous logguer afin d'obtenir un JWT.");
+    }
+}
+
+
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -2,19 +2,19 @@ module.exports = function(app) {
     let user = require('../controllers/userController');
 
     app.route('/users')
-        .get(user.list_all_users)
+        .get(user.verifyToken, user.list_all_users)
         .post(user.create_user);
 
     app.route('/users/:name')
-        .put(user.update_user)
-        .get(user.read_user)
-        .delete(user.delete_user);
+        .put(user.verifyToken, user.update_user)
+        .get(user.verifyToken, user.read_user)
+        .delete(user.verifyToken, user.delete_user);
 
     app.route('/users/:name/pokemons')
-        .get(user.list_pokemons_user)
-        .post(user.create_pokemon_user);
+        .get(user.verifyToken, user.list_pokemons_user)
+        .post(user.verifyToken, user.create_pokemon_user);
 
     app.route('/users/:name/pokemons/:Id')
-        .get(user.read_pokemon_user)
-        .delete(user.delete_pokemon_user);
+        .get(user.verifyToken, user.read_pokemon_user)
+        .delete(user.verifyToken, user.delete_pokemon_user);
 };
